f6b1e80adce547ef5e1fbc4da7d2e7ff7e1e68df
Navid_Rahmani <navidrahmani770@gmail.com>
2024-11-22 05:43:11 -0500

    singleton

---
+++ hw4/ConnectionHolder.java
@@ -0,0 +1,27 @@
+package hw4;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ConnectionHolder {
+
+    private static final Logger log = Logger.getLogger(ConnectionHolder.class.getName());
+
+    private static Connection connection;
+
+    public static Connection getInstance() {
+        if (connection == null) {
+            try {
+                connection = DriverManager.getConnection("jdbc:h2:~/test", "", "");
+            } catch (SQLException e) {
+                log.log(Level.SEVERE, "Connect error", e);
+                throw new RuntimeException(e);
+            }
+        }
+
+        return connection;
+    }
+}
